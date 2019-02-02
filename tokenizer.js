35f88b0fea2b21027dca06034c6adcae4987eff8
daixinye <denight@qq.com>
2019-02-02 22:28:56 +0800

    add paren

--- tokenizer.js
+++ tokenizer.js
@@ -30,7 +30,7 @@
 
 const WHITESPACE = /\s/;
 const NEWLINE = /\n/;
-const STRING = /[a-zA-Z0-9.:;=+/\u4e00-\u9fa5\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b]/;
+const STRING = /[a-zA-Z0-9().:;=+/\u4e00-\u9fa5\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b]/;
 
 function tokenizer(input) {
   let current = 0;
@@ -98,7 +98,7 @@ module.exports = tokenizer;
 
 if (require.main === module) {
   const fs = require("fs");
-  const input = fs.readFileSync("./.hostsxy.simple", {
+  const input = fs.readFileSync("./.hostsxy", {
     encoding: "utf-8"
   });
 
