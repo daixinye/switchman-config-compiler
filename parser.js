1ee60038a2eb8819a76ccb57a1724f5fe078a262
daixinye <denight@qq.com>
2019-02-03 01:42:57 +0800

    resolve #2

--- parser.js
+++ parser.js
@@ -28,30 +28,82 @@ Output:
   },
   {
     "type": "rule",
-    "value": {
-      "target": "127.0.0.1:8000",
-      "origin": "http://www.daixinye.com/path",
-      "headers": [
-        {
-          "operator": "=",
-          "name": "host",
-          "value": "test.daixinye.com"
-        },
-        {
-          "operator": "+",
-          "name": "cookie",
-          "value": "hostsxy=true;"
-        }
-      ]
+    "value": ["127.0.0.1:8000",http://www.daixinye.com/path"]
     }
+  },
+  {
+    "type": "subrule",
+    "value": [=host","test.daixinye.com"]
+  },
+  {
+    "type": "subrule",
+    "value": ["+cookie", "hostsxy=true;"]
   }
 ]
 ```
  */
 
+const OPERATOR = /[+=]/;
 function parser(tokens) {
-  const ast = {};
+  const ast = [];
+  let current = 0;
+  let newline = true;
 
+  function walk() {
+    let token = tokens[current];
+
+    if (token.type === "newline") {
+      newline = true;
+      current++;
+      return null;
+    }
+
+    if (token.type === "hash") {
+      let annotation = "";
+      token = tokens[++current];
+      while (token.type !== "newline" && current < tokens.length) {
+        annotation += token.value;
+        token = tokens[++current];
+      }
+      return {
+        type: "annotation",
+        value: annotation
+      };
+    }
+
+    if (token.type === "string" && newline) {
+      let prev = tokens[current];
+      let next = tokens[++current];
+      if (next.type === "string") {
+        newline = false;
+        current++;
+        return { type: "rule", value: [prev.value, next.value] };
+      } else {
+        throw new TypeError(token.type);
+      }
+    }
+
+    if (token.type === "tab") {
+      if (newline) {
+        current++;
+        let result = walk();
+        if (result) {
+          result.type = "sub_" + result.type;
+        }
+        return result;
+      } else {
+        current++;
+        return null;
+      }
+    }
+
+    throw new TypeError(token.type);
+  }
+
+  while (current < tokens.length) {
+    let rule = walk();
+    rule && ast.push(rule);
+  }
   return ast;
 }
 
@@ -60,7 +112,7 @@ module.exports = parser;
 if (require.main === module) {
   const tokenizer = require("./tokenizer");
   const fs = require("fs");
-  const input = fs.readFileSync("./.hostsxy.simple", {
+  const input = fs.readFileSync("./.hostsxy", {
     encoding: "utf-8"
   });
 
